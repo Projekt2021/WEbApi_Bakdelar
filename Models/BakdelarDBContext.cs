3b970c8c4ed958915231c801437e7b4f6c947769
robinfransson <71066647+robinfransson@users.noreply.github.com>
2021-02-09 12:45:35 +0100

    Initial commit

---
+++ Models/BakdelarDBContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TestApi.Models
+{
+    public partial class BakdelarDBContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(@"D:\.net programmering lektioner\TestApi")
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                optionsBuilder.UseSqlServer(configuration.GetConnectionString("BakdelarDB"));
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Product");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+        public BakdelarDBContext()
+        {
+        }
+
+        public BakdelarDBContext(DbContextOptions<BakdelarDBContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public string ImageLink { get; set; }
+        public double SalePrice { get; set; }
+        public string Secret { get; set; }
+
+    }
+}
